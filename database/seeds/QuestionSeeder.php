275de5cc93e060fcec87ebdade9bb9030e7bb30b
Nezimed <nezimed@gmail.com>
2020-08-05 19:51:27 +0100

    minor changes

--- database/seeds/QuestionSeeder.php
+++ database/seeds/QuestionSeeder.php
@@ -14,31 +14,31 @@ class QuestionSeeder extends Seeder
     {
         Question::insert([
             [
-                'question' => 'Numa escala de {min} a {max}, como descreve o desempenho do gestor de projecto?',
+                'text' => 'Numa escala de {min} a {max}, como descreve o desempenho do gestor de projecto?',
                 'type' => 'range',
                 'min' => 0,
                 'max' => 10
             ],
             [
-                'question' => 'Numa escala de {min} a {max} como descreve o desempenho do account manager no projecto?',
+                'text' => 'Numa escala de {min} a {max} como descreve o desempenho do account manager no projecto?',
                 'type' => 'range',
                 'min' => 0,
                 'max' => 10
             ],
             [
-                'question' => 'Numa escala de {min} a {max}, descreva a sua experiência geral no projecto?',
+                'text' => 'Numa escala de {min} a {max}, descreva a sua experiência geral no projecto?',
                 'type' => 'range',
                 'min' => 0,
                 'max' => 10
             ],
             [
-                'question' => 'Que melhorias sugeria para o projecto?',
+                'text' => 'Que melhorias sugeria para o projecto?',
                 'type' => 'free',
                 'min' => null,
                 'max' => null
             ],
             [
-                'question' => 'Que melhorias sugeria para a empresa?',
+                'text' => 'Que melhorias sugeria para a empresa?',
                 'type' => 'free',
                 'min' => null,
                 'max' => null
